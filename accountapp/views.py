50f2c354a1ce2e64f92ff28e540201f39885cedd
yewon <ye7648@gmail.com>
2023-05-16 01:14:26 +0900

    django course 26 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.models import HelloWorld
-from accountapp.templates.accountapp.forms import AccountUpdateForm
+from accountapp.forms import AccountUpdateForm
 
 
 def hello_world(request):
@@ -45,5 +45,11 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp/hello_world.html')
+    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
